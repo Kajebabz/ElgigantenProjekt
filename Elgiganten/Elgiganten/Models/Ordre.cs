dc6bd678a06535c88919bd08b84021667fd0f2f3
KurbaniKoc <117358161+KurbaniKoc@users.noreply.github.com>
2022-12-08 13:47:31 +0100

    Ordre

---
+++ Elgiganten/Elgiganten/Models/Ordre.cs
@@ -0,0 +1,29 @@
+﻿namespace Elgiganten.Models
+{
+    public class Ordre
+    {  
+        public double TotalPrice { get; set; }
+        public int Id { get; set; }
+        public string MemberName { get; set; }
+        public DateOnly Date { get; set; }
+        
+
+        public Ordre()
+        {
+        }
+
+        public Ordre(double totalPrice, int id, string memberName, DateOnly date)
+        {
+            TotalPrice = totalPrice;
+            Id = id;
+            MemberName = memberName;
+            Date = date;
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(TotalPrice)}={TotalPrice.ToString()}, {nameof(Id)}={Id.ToString()}, {nameof(MemberName)}={MemberName}, {nameof(Date)}={Date.ToString()}}}";
+        }
+    }
+}
+
