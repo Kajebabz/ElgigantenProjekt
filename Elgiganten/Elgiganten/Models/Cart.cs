38c372810a1b312af10ecba6f46e897991763628
Freakkoe <mads.g.j@live.dk>
2022-12-09 11:24:04 +0100

    Cart

---
+++ Elgiganten/Elgiganten/Models/Cart.cs
@@ -0,0 +1,23 @@
+﻿namespace Elgiganten.Models
+{
+        public class CartItem
+        {
+            public CartItem()
+            {
+            }
+
+            public CartItem(int id, string name, int quantity, decimal price)
+            {
+                Id = id;
+                Name = name;
+                Quantity = quantity;
+                Price = price;
+            }
+
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Quantity { get; set; }
+            public decimal Price { get; set; }
+        }
+    
+}
